d24c6d511b0ae08f99dc8d07f4dfb99f02bca086
Garrett Vargas <gsdriver@live.com>
2018-11-10 05:13:13 -0800

    Update German content

--- lambda/custom/intents/BetAmount.js
+++ lambda/custom/intents/BetAmount.js
@@ -103,7 +103,8 @@ module.exports = {
           // On to the next player
           attributes.temp.changingBets++;
           reprompt = res.strings.CHANGEBETS_REPROMPT;
-          speech += (utils.readPlayerName(event.request.locale, attributes, attributes.temp.changingBets) + reprompt);
+          speech += (utils.readPlayerName(event.request.locale, attributes,
+            attributes.temp.changingBets) + reprompt);
 
           // Color this player if they have a button associated
           buttons.disableButtons(handlerInput);
